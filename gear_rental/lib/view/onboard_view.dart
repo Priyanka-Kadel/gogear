d6ce30e8d6c529fc9d2edbf112a1453f69281e81
Priyanka-Kadel <kadelpriyanka21@gmail.com>
2024-12-13 16:41:40 +0545

    a

--- gear_rental/lib/view/onboard_view.dart
+++ gear_rental/lib/view/onboard_view.dart
@@ -9,11 +9,6 @@ class OnboardView extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Image.asset(
-              'gear_rental\assets\images\Screenshot 2024-11-29 205129.png',
-              width: 150,
-              height: 150,
-              ),
             Text(
               'Welcome to GoGear',
               style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
