3e79e5a23a774f0d633d9739f151fc5fe95dbba5
Priyanka-Kadel <kadelpriyanka21@gmail.com>
2025-02-07 16:39:05 +0545

    failure repository test 3

--- gear_rental/test/core/error/failure_test.dart
+++ gear_rental/test/core/error/failure_test.dart
@@ -28,5 +28,18 @@ void main() {
       expect(
           failure1, isNot(equals(failure3))); // Different message or statusCode
     });
+    test('LocalDatabaseFailure equality based on message', () {
+      // Arrange
+      const message1 = 'Database failure';
+      const message2 = 'Database error';
+
+      const failure1 = LocalDatabaseFailure(message: message1);
+      const failure2 = LocalDatabaseFailure(message: message1);
+      const failure3 = LocalDatabaseFailure(message: message2);
+
+      // Act & Assert
+      expect(failure1, equals(failure2)); // Same message
+      expect(failure1, isNot(equals(failure3))); // Different message
+    });
   });
 }
