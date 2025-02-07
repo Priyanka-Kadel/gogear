af14e56fa4d25d809f81f50bb569e3c4ba77df22
Priyanka-Kadel <kadelpriyanka21@gmail.com>
2025-02-07 17:10:45 +0545

    code enhancement(auth repository)

--- gear_rental/lib/features/auth/domain/repository/auth_repository.dart
+++ gear_rental/lib/features/auth/domain/repository/auth_repository.dart
@@ -1,12 +1,12 @@
-// import 'package:dartz/dartz.dart';
+import 'package:dartz/dartz.dart';
 
-// import '../../../../core/error/failure.dart';
-// import '../entity/auth_entity.dart';
+import '../../../../core/error/failure.dart';
+import '../entity/auth_entity.dart';
 
-// abstract interface class IAuthRepository {
-//   Future<Either<Failure, void>> registerUser(AuthEntity user);
+abstract interface class IAuthRepository {
+  Future<Either<Failure, void>> registerUser(AuthEntity user);
 
-//   Future<Either<Failure, String>> loginUser(String email, String password);
+  Future<Either<Failure, String>> loginUser(String email, String password);
 
-//   Future<Either<Failure, AuthEntity>> getCurrentUser();
-// }
+  Future<Either<Failure, AuthEntity>> getCurrentUser();
+}
