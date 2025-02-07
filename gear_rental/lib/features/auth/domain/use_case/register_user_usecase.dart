ebae65c32ede6984c4f7d905bb176528d198e64f
Priyanka-Kadel <kadelpriyanka21@gmail.com>
2025-02-07 17:07:19 +0545

    code enhancement(register usecase)

--- gear_rental/lib/features/auth/domain/use_case/register_user_usecase.dart
+++ gear_rental/lib/features/auth/domain/use_case/register_user_usecase.dart
@@ -1,43 +1,45 @@
-// import 'package:dartz/dartz.dart';
-// import 'package:equatable/equatable.dart';
+import 'package:dartz/dartz.dart';
+import 'package:equatable/equatable.dart';
 
-// import '../../../../app/usecase/usecase.dart';
-// import '../../../../core/error/failure.dart';
-// import '../entity/auth_entity.dart';
-// import '../repository/auth_repository.dart';
+import '../../../../app/usecase/usecase.dart';
+import '../../../../core/error/failure.dart';
+import '../entity/auth_entity.dart';
+import '../repository/auth_repository.dart';
 
-// class RegisterUserParams extends Equatable {
-//   final String email;
-//   final String username;
-//   final String password;
+class RegisterUserParams extends Equatable {
+  final String email;
+  final String username;
+  final String password;
 
-//   const RegisterUserParams(
-//       {required this.email,
-//       required this.username,
-//       required this.password,});
+  const RegisterUserParams({
+    required this.email,
+    required this.username,
+    required this.password,
+  });
 
-//   //intial constructor
-//   const RegisterUserParams.initial(
-//       {required this.email,
-//       required this.username,
-//       required this.password,});
+  //intial constructor
+  const RegisterUserParams.initial({
+    required this.email,
+    required this.username,
+    required this.password,
+  });
 
-//   @override
-//   List<Object?> get props => [email, username, password];
-// }
+  @override
+  List<Object?> get props => [email, username, password];
+}
 
-// class RegisterUseCase implements UsecaseWithParams<void, RegisterUserParams> {
-//   final IAuthRepository repository;
+class RegisterUseCase implements UsecaseWithParams<void, RegisterUserParams> {
+  final IAuthRepository repository;
 
-//   RegisterUseCase(this.repository);
+  RegisterUseCase(this.repository);
 
-//   @override
-//   Future<Either<Failure, void>> call(RegisterUserParams params) {
-//     final authEntity = AuthEntity(
-//       email: params.email,
-//       username: params.username,
-//       password: params.password,
-//     );
-//     return repository.registerUser(authEntity);
-//   }
-// }
+  @override
+  Future<Either<Failure, void>> call(RegisterUserParams params) {
+    final authEntity = AuthEntity(
+      email: params.email,
+      username: params.username,
+      password: params.password,
+    );
+    return repository.registerUser(authEntity);
+  }
+}
