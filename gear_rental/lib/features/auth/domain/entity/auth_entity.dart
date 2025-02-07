31ab5f9bb0631370017c9ab02cf7641527b99ed5
Priyanka-Kadel <kadelpriyanka21@gmail.com>
2025-02-07 17:08:47 +0545

    changes in auth entity

--- gear_rental/lib/features/auth/domain/entity/auth_entity.dart
+++ gear_rental/lib/features/auth/domain/entity/auth_entity.dart
@@ -1,18 +1,18 @@
-import 'package:equatable/equatable.dart';
+// import 'package:equatable/equatable.dart';
 
-class AuthEntity extends Equatable {
-  final String? userId;
-  final String email;
-  final String username;
-  final String password;
+// class AuthEntity extends Equatable {
+//   final String? userId;
+//   final String email;
+//   final String username;
+//   final String password;
 
-  const AuthEntity({
-    this.userId,
-    required this.email,
-    required this.username,
-    required this.password,
-  });
+//   const AuthEntity({
+//     this.userId,
+//     required this.email,
+//     required this.username,
+//     required this.password,
+//   });
 
-  @override
-  List<Object?> get props => [userId, email, username, password];
-}
+//   @override
+//   List<Object?> get props => [userId, email, username, password];
+// }
