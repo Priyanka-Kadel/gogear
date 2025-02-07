81cafd6c45cb78140a5747eb182bcde0e0c9be9f
Priyanka-Kadel <kadelpriyanka21@gmail.com>
2025-02-07 17:05:41 +0545

    code enhancement(login usecase)

--- gear_rental/lib/features/auth/domain/use_case/login_usecase.dart
+++ gear_rental/lib/features/auth/domain/use_case/login_usecase.dart
@@ -1,36 +1,36 @@
-// import 'package:dartz/dartz.dart';
-// import 'package:equatable/equatable.dart';
+import 'package:dartz/dartz.dart';
+import 'package:equatable/equatable.dart';
 
-// import '../../../../app/usecase/usecase.dart';
-// import '../../../../core/error/failure.dart';
-// import '../repository/auth_repository.dart';
+import '../../../../app/usecase/usecase.dart';
+import '../../../../core/error/failure.dart';
+import '../repository/auth_repository.dart';
 
-// class LoginParams extends Equatable {
-//   final String email;
-//   final String password;
+class LoginParams extends Equatable {
+  final String email;
+  final String password;
 
-//   const LoginParams({
-//     required this.email,
-//     required this.password,
-//   });
+  const LoginParams({
+    required this.email,
+    required this.password,
+  });
 
-//   // Initial Constructor
-//   const LoginParams.initial()
-//       : email = '',
-//         password = '';
+  // Initial Constructor
+  const LoginParams.initial()
+      : email = '',
+        password = '';
 
-//   @override
-//   List<Object> get props => [email, password];
-// }
+  @override
+  List<Object> get props => [email, password];
+}
 
-// class LoginUseCase implements UsecaseWithParams<String, LoginParams> {
-//   final IAuthRepository repository;
+class LoginUseCase implements UsecaseWithParams<String, LoginParams> {
+  final IAuthRepository repository;
 
-//   LoginUseCase(this.repository);
+  LoginUseCase(this.repository);
 
-//   @override
-//   Future<Either<Failure, String>> call(LoginParams params) {
-//     // IF api then store token in shared preferences
-//     return repository.loginUser(params.email, params.password);
-//   }
-// }
+  @override
+  Future<Either<Failure, String>> call(LoginParams params) {
+    // IF api then store token in shared preferences
+    return repository.loginUser(params.email, params.password);
+  }
+}
