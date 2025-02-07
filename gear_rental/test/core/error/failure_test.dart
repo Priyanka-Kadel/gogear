c153bcd32df74b999fc6de5ba48de7dc01fccb06
Priyanka-Kadel <kadelpriyanka21@gmail.com>
2025-02-07 16:38:36 +0545

    failure repository test 2

--- gear_rental/test/core/error/failure_test.dart
+++ gear_rental/test/core/error/failure_test.dart
@@ -12,5 +12,21 @@ void main() {
       expect(failure.message, message);
       expect(failure.props, [message]);
     });
+    test('ApiFailure equality based on message and statusCode', () {
+      // Arrange
+      const message1 = 'API request failed';
+      const statusCode1 = 404;
+      const message2 = 'Not found';
+      const statusCode2 = 404;
+
+      const failure1 = ApiFailure(statusCode1, message: message1);
+      const failure2 = ApiFailure(statusCode1, message: message1);
+      const failure3 = ApiFailure(statusCode2, message: message2);
+
+      // Act & Assert
+      expect(failure1, equals(failure2)); // Same message and statusCode
+      expect(
+          failure1, isNot(equals(failure3))); // Different message or statusCode
+    });
   });
 }
