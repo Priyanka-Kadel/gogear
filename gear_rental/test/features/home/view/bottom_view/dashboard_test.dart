4dfc3f9947bc7c07ad27769bcadc7fb991997ce7
Priyanka-Kadel <kadelpriyanka21@gmail.com>
2025-03-05 12:08:31 +0545

    8 widget tests done

---
+++ gear_rental/test/features/home/view/bottom_view/dashboard_test.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:gear_rental/features/dashboard_page.dart';
+import 'package:gear_rental/features/home/view/bottom_view/all_gears_page.dart';
+
+void main() {
+  testWidgets('DashboardPage has AppBar with correct title',
+      (WidgetTester tester) async {
+    await tester.pumpWidget(MaterialApp(home: DashboardPage()));
+
+    // Verify if the title of the AppBar is displayed
+    expect(find.text('Dashboard'), findsOneWidget);
+  });
+
+  testWidgets('DashboardPage navigates to AllGearsPage when gear is clicked',
+      (WidgetTester tester) async {
+    await tester.pumpWidget(MaterialApp(home: DashboardPage()));
+
+    // Tap on a gear item
+    await tester.tap(find.byType(GestureDetector).first);
+    await tester.pumpAndSettle();
+
+    // Verify that AllGearsPage is displayed after navigation
+    expect(find.byType(AllGearsPage), findsOneWidget);
+  });
+}
